f3cab693c599af47e2ac9ddb97fee0da0ecfc36f
dustyo-O <alexander.shleyko@yumalabs.ru>
2016-08-12 22:07:29 +0300

    +main layout proto

--- desktop.bundles/interface/interface.bemjson.js
+++ desktop.bundles/interface/interface.bemjson.js
@@ -1,3 +1,28 @@
 /**
  * Created by dusty on 12.08.16.
  */
+({
+    block : 'page',
+    title : 'interface',
+    head : [
+        { elem : 'css', url : 'interface.min.css' }
+    ],
+    scripts : [{ elem : 'js', url : 'interface.min.js' }],
+    mods : { theme : 'islands' },
+    content : [
+        {
+            block : 'header',
+            content: 'header here'
+        },
+        {
+            block : 'feed',
+            content: 'feed here'
+        },
+        {
+            block : 'footer',
+            content: 'footer here'
+
+        }
+
+    ]
+})
