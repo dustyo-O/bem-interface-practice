90d9c2022e2d10220fd29f3e3e7f329bc42bb3fa
Smolnikovp <smolnikovp@gmail.com>
2016-08-12 22:13:03 +0300

    some changes

--- desktop.bundles/interface/interface.bemjson.js
+++ desktop.bundles/interface/interface.bemjson.js
@@ -7,16 +7,13 @@ module.exports = {
         { elem : 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1' } },
         { elem : 'css', url : 'interface.min.css' },
         { elem: 'js', url: 'interface.bemhtml.js' },
-        { }
+        { block: 'font-awesome' }
     ],
     scripts: [{ elem : 'js', url : 'interface.min.js' }],
     mods : { theme : 'islands' },
     content : [
         {
             block: 'footer'
-        },
-        {
-            block: 'someItem'
         }
     ]
 };
